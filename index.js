6e6014ac0d018abce45823c50290e3cdff0d8e82
chroc <chroc7@gmail.com>
2023-02-04 10:15:57 -0500

    deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler.js');
 
@@ -7,8 +8,20 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const whiteList = ['http://localhost:8080', 'https://myapp.com'];
+const options = {
+    origin: (origin, callback) => {
+        if (whiteList.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('not allowed'));
+        }
+    }
+};
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('Hello Express - My Store!');
 });
 
 // Define routing
